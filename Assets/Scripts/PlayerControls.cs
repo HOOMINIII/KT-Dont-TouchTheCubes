c06bafe9f4c63195bc133a17f90e936802ca0c86
iam9116 <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-05-23 17:40:15 -0400

    hola
    i left of on 12

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Rigidbody")]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
