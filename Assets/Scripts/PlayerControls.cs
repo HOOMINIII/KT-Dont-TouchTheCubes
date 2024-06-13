5ba076ced70d568b22efcd0be32b92bf575bf63d
iam9116 <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-06-13 17:45:56 -0400

    on step 13 =(

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
-    [Header("Rigidbody")] 
+    [Header("Rigidbody")]
     public Rigidbody rb;
 
     // Start is called before the first frame update
@@ -13,7 +13,6 @@ public class PlayerControls : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         Time.timeScale = 1f;
-
     }
 
     // Update is called once per frame
@@ -23,7 +22,7 @@ public class PlayerControls : MonoBehaviour
         Time.timeScale += Time.fixedDeltaTime * 0.01f;
         rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizontal") * 10f * Time.deltaTime);
         rb.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -30f, 30f),
-            transform.position.y, Mathf.Clamp(transform.position.z, -30f, 30f));
+        
+
     }
 }
