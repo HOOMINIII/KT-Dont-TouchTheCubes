4962d262a6e52c13e7530c7c93286b4bfc6ce25e
CodeNinjasEtobicoke <judeolmo@gmail.com>
2024-06-22 14:50:32 -0400

    Game Restarted=(

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,24 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
-    [Header("Rigidbody")]
-    public Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation *= Quaternion.Euler(0, 0, 7 * Time.deltaTime);
-        Time.timeScale += Time.fixedDeltaTime * 0.01f;
-        rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizontal") * 10f * Time.deltaTime);
-        rb.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
-        
 
     }
 }
